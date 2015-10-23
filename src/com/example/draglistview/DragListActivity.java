398b4f110dfbbecc86a8249f7be452556e8b917e
lantian0314 <384289339@qq.com>
2015-10-23 14:02:13 +0800

    添加DragListView类的方法

--- src/com/example/draglistview/DragListActivity.java
+++ src/com/example/draglistview/DragListActivity.java
@@ -16,63 +16,75 @@ import android.widget.TextView;
 
 public class DragListActivity extends Activity {
 
-	private List<String> list=null;
-	
-	//存放分组标签
-	private static List<String> groupKey=new ArrayList<String>();
-	
-	//分组一
-	private List<String> firstList=new ArrayList<String>();
-	//分组二
-	private List<String> secondList=new ArrayList<String>();
-	
+	private List<String> list = null;
+
+	// 存放分组标签
+	private static List<String> groupKey = new ArrayList<String>();
+
+	// 分组一
+	private List<String> firstList = new ArrayList<String>();
+	// 分组二
+	private List<String> secondList = new ArrayList<String>();
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.drag_list_main);
-		
-		//初始化数据
+
+		// 初始化数据
 		initData();
-		
-		
+
+		DragListView dragListView = (DragListView) findViewById(R.id.DragGroupList);
+		DragListAdapter madapter = new DragListAdapter(this, 0, list);
+		dragListView.setAdapter(madapter);
 	}
 
 	private void initData() {
-		list=new ArrayList<String>();
-		
-		//groupKey存放分组标签
+		list = new ArrayList<String>();
+
+		// groupKey存放分组标签
 		groupKey.add("A组");
 		groupKey.add("B组");
-		
+
 		for (int i = 0; i < 3; i++) {
-			firstList.add("A选项"+i);
+			firstList.add("A选项" + i);
 		}
 		list.add("A组");
 		list.addAll(firstList);
-		
+
 		for (int i = 0; i < 3; i++) {
-			secondList.add("B选项"+i);
+			secondList.add("B选项" + i);
 		}
 		list.add("B组");
 		list.addAll(secondList);
 	}
-	
-	private class DragListAdapter extends ArrayAdapter<String>{
 
-		public DragListAdapter(Context context, int resource,
-				List<String> objects) {
-			super(context, resource, objects);
-			
+	public class DragListAdapter extends ArrayAdapter<String> {
+
+		private List<String> mList;
+
+		public DragListAdapter(Context context, int resource, List<String> list) {
+			super(context, resource, list);
+			mList = list;
 		}
-		
+
+		public List<String> getList() {
+			return list;
+		}
+
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
-			View view=convertView;
-			if (view==null) {
-				view=LayoutInflater.from(getContext()).inflate(R.layout.drag_list_iteam, null);
-				TextView iteamText=(TextView) view.findViewById(R.id.Drag_list_iteam_text);
-				iteamText.setText(list.get(position));
+			View view = convertView;
+			if (view == null) {
+				view = LayoutInflater.from(getContext()).inflate(
+						R.layout.drag_list_iteam, null);
+				
+			}else {
+				view=convertView;
 			}
+			TextView iteamText = (TextView) view
+					.findViewById(R.id.Drag_list_iteam_text);
+			iteamText.setText(getItem(position));
 			return view;
 		}
 	}
