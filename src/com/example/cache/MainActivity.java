0804290d9401324fba127d2d36fa53c235029880
xingyatong <384289339@qq.com>
2015-11-04 16:27:06 +0800

    图片的现在改为异步

--- src/com/example/cache/MainActivity.java
+++ src/com/example/cache/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.cache;
 
 import com.example.listviewtest.R;
+import com.example.utils.Tools;
 
+import android.R.integer;
+import android.R.string;
 import android.app.Activity;
 import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.ImageView;
 
 public class MainActivity extends Activity {
@@ -13,39 +18,50 @@ public class MainActivity extends Activity {
 	private ImageFileCache fileCache;
 	private ImageView imageView;
 	private static Bitmap result;
+	private static int count = 0;
+	private static int max_count = 5;
+	private static String murl = null;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.cache_main);
 		memoryCache = new ImageMemoryCache(getApplicationContext());
-		fileCache=new ImageFileCache();
+		fileCache = new ImageFileCache();
 		imageView = (ImageView) findViewById(R.id.cache_img);
 		Bitmap bitmap = getBitmap("http://f.hiphotos.baidu.com/album/w%3D2048/sign=7aa167f79f2f07085f052d00dd1cb999/472309f7905298228f794c7bd6ca7bcb0b46d4c4.jpg");
 		imageView.setImageBitmap(bitmap);
 	}
 
 	private Bitmap getBitmap(final String url) {
-	 result = memoryCache.getBitmapFromCache(url);
+		murl = url;
+		result = memoryCache.getBitmapFromCache(url);
 		if (result == null) {
-			result=fileCache.getImage(url);
-			if (result==null) {
-				// ¥”Õ¯¬ÁªÒ»°
-				new Thread(new Runnable() {	
+			result = fileCache.getImage(url);
+			if (result == null) {
+				new  AsyncTask<String, Void, Bitmap>(){
 					@Override
-					public void run() {
-						result = ImageGetFromHttp.downloadBitmap(url);	
+					protected Bitmap doInBackground(String... params) {
+						result = ImageGetFromHttp.downloadBitmap(url);
+						return result;
 					}
-				}).start();
-				
-				if (result != null) {
-					fileCache.saveBitmap(result, url);
-					memoryCache.addBitmapToCache(url, result);
-				}
-			}else {
+					
+					@Override
+					protected void onPostExecute(Bitmap result) {
+						if (result != null) {
+							fileCache.saveBitmap(result, url);
+							memoryCache.addBitmapToCache(url, result);
+							imageView.setImageBitmap(result);
+						}
+					}
+				}.execute(url);
+
+			} else {
 				memoryCache.addBitmapToCache(url, result);
 			}
-		
+
 		}
 		return result;
 	}
+
 }
