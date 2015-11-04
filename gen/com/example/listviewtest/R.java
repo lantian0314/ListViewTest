3ff6a919ddb23313dff9e34c14da6c3c4d942cce
xingyatong <384289339@qq.com>
2015-11-04 15:05:52 +0800

    图片的三级缓存,内存、文件、网络

--- gen/com/example/listviewtest/R.java
+++ gen/com/example/listviewtest/R.java
@@ -24,14 +24,15 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int DragGroupList=0x7f080009;
-        public static final int Drag_Main_Text=0x7f080008;
-        public static final int Drag_list_iteam_Image=0x7f080007;
-        public static final int Drag_list_iteam_text=0x7f080006;
+        public static final int DragGroupList=0x7f08000a;
+        public static final int Drag_Main_Text=0x7f080009;
+        public static final int Drag_list_iteam_Image=0x7f080008;
+        public static final int Drag_list_iteam_text=0x7f080007;
         public static final int GroupList=0x7f080001;
         public static final int Iteam_Tag_Text=0x7f080000;
-        public static final int ItemText=0x7f08000a;
-        public static final int action_settings=0x7f08000b;
+        public static final int ItemText=0x7f08000b;
+        public static final int action_settings=0x7f08000c;
+        public static final int cache_img=0x7f080006;
         public static final int cancel=0x7f080003;
         public static final int execute=0x7f080002;
         public static final int progress=0x7f080004;
@@ -40,9 +41,10 @@ public final class R {
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int async=0x7f030001;
-        public static final int drag_list_iteam=0x7f030002;
-        public static final int drag_list_main=0x7f030003;
-        public static final int list_iteam=0x7f030004;
+        public static final int cache_main=0x7f030002;
+        public static final int drag_list_iteam=0x7f030003;
+        public static final int drag_list_main=0x7f030004;
+        public static final int list_iteam=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- src/com/example/utils/Tools.java
+++ src/com/example/utils/Tools.java
@@ -1,10 +1,35 @@
 package com.example.utils;
 
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.io.Writer;
+
 import android.util.Log;
 
 public class Tools {
 
+	private static String TAG="json.sky";
 	public static void printLog(String string){
-		Log.d("json.sky", string);
+		Log.d(TAG, string);
+	}
+	
+	public static void e(Exception e){
+		try {
+			if (e!=null) {
+				Log.e(TAG, getStackTrace(e));
+			}
+		} catch (Exception e2) {
+			Log.e(TAG,getStackTrace(e2));
+		}
+	}
+
+	private static String getStackTrace(Throwable mThrowable) {
+		if (mThrowable!=null) {
+			Writer mWriter=new StringWriter();
+			PrintWriter printWriter=new PrintWriter(mWriter);
+			mThrowable.printStackTrace(printWriter);
+			return mWriter.toString();
+		}
+		return null;
 	}
 }
