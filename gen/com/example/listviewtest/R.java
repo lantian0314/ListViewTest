ba9876c9ea0f6026067c9538866d7e7ab75e8f43
lantian0314 <384289339@qq.com>
2015-11-11 16:30:19 +0800

    单词查询

--- gen/com/example/listviewtest/R.java
+++ gen/com/example/listviewtest/R.java
@@ -31,11 +31,14 @@ public final class R {
         public static final int GroupList=0x7f080001;
         public static final int Iteam_Tag_Text=0x7f080000;
         public static final int ItemText=0x7f08000b;
-        public static final int action_settings=0x7f08000c;
+        public static final int action_settings=0x7f08000f;
+        public static final int btn_trans=0x7f08000d;
         public static final int cache_img=0x7f080006;
         public static final int cancel=0x7f080003;
+        public static final int edit_trans=0x7f08000c;
         public static final int execute=0x7f080002;
         public static final int progress=0x7f080004;
+        public static final int text_result=0x7f08000e;
         public static final int textview=0x7f080005;
     }
     public static final class layout {
@@ -45,6 +48,7 @@ public final class R {
         public static final int drag_list_iteam=0x7f030003;
         public static final int drag_list_main=0x7f030004;
         public static final int list_iteam=0x7f030005;
+        public static final int translation=0x7f030006;
     }
     public static final class menu {
         public static final int main=0x7f070000;
---
+++ src/com/example/Translation/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.Translation;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.example.listviewtest.R;
+import com.example.utils.Tools;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+	protected static final int MSG_DOWNLOAD = 10;
+	private EditText edit_trans;
+	private TextView text_result;
+	private Button btn_trans;
+	private String shuru;
+	
+	private JSONObject object;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.translation);
+		edit_trans = (EditText) findViewById(R.id.edit_trans);
+		btn_trans = (Button) findViewById(R.id.btn_trans);
+		text_result=(TextView) findViewById(R.id.text_result);
+		doClick();
+	}
+
+	/**
+	 * 点击监听事件
+	 */
+	private void doClick() {
+		btn_trans.setOnClickListener(new OnClickListener() {	
+			@Override
+			public void onClick(View v) {
+				shuru=edit_trans.getText().toString().trim();
+				shuru=URLEncoder.encode(shuru);//实现编码格式化
+				if (TextUtils.isEmpty(shuru)) {
+					edit_trans.setText("内容不能为空");
+				}else {
+					new Thread(new Runnable() {	
+						@Override
+						public void run() {
+							try {
+								object=new JSONObject(readParse("http://fanyi.youdao.com/openapi.do?keyfrom=woainixiaomao&key=1491298559&type=data&doctype=json&version=1.1&q="+shuru));
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+							mHandler.sendEmptyMessage(MSG_DOWNLOAD);//发送消息，加载成功
+						}
+					}).start();
+				}
+			}
+		});
+	}
+	
+	private  String readParse(String urlPath) throws Exception {  
+		ByteArrayOutputStream outStream = new ByteArrayOutputStream();  
+		byte[] data = new byte[1024];  
+		int len = 0;  
+		URL url = new URL(urlPath);  
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();  
+		InputStream inStream = conn.getInputStream();  
+		while ((len = inStream.read(data)) != -1) {  
+			outStream.write(data, 0, len);  
+		}  
+		inStream.close();  
+		return new String(outStream.toByteArray());
+	}
+	
+	private void show() {
+		try {
+			JSONArray array=object.getJSONArray("translation");
+			for (int i = 0; i < array.length(); i++) {
+				text_result.setText(array.getString(i));
+				text_result.setTextColor(Color.BLACK);
+			}
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	};
+	 
+	private Handler mHandler = new Handler() {
+		public void handleMessage(android.os.Message msg) {
+			switch (msg.what) {
+			case MSG_DOWNLOAD:
+				try {
+				show();	
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				break;
+
+			default:
+				break;
+			}
+		}
+	};
+}
